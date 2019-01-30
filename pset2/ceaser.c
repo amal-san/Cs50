274fb4563e623b1ecb5d837269db6b5cc0dd6c2d
Amalsanthoshp <amaldemaniax@gmail.com>
2019-01-30 13:39:18 +0530

    ceaser

---
+++ pset2/ceaser.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+   char str[100];
+   printf("Enter the plaintext: \n");
+   scanf("%s",str);
+   int length = strlen(str);
+   char c[100];
+   int i=0;
+   char d[100];
+   printf("Plaintext: %s\n",str);
+   for (i=0;i<length;i++)
+   {  
+     if(str[i] >='a' && str[i] <='z')
+	{
+    	 if(str[i] !=EOF)
+    	  {
+		if(d[i] = (str[i] + 12 ) <= 'z')
+		  {
+			d[i] += str[i] + 12;
+		  }
+		else
+		  {
+			d[i] += str[i] - 12;    
+   
+		  }
+    	   }
+       }  
+   }
+   printf("Ciphertext :%s", d );
+   return 0;
+}
