10c3e5b38038b2ba0bc36f1c82539bce88fb3d23
Amalsanthoshp <amaldemaniax@gmail.com>
2019-02-02 15:02:27 +0530

    cash.c

--- pset1/cash.c
+++ pset1/cash.c
@@ -16,10 +16,11 @@ int main()
     }
     while (cents <= 0);
 	
-    int quarters = cents / 25; /*quater is 25c */
-    int dimes = (cents % 25) / 10; /*dimes is 10c */
-    int nickels = ((cents % 25) % 10) / 5; /
-    int pennies = ((cents % 25) % 10) % 5;
+    int quarters = cents / 25; /*quaters are 25c */
+    int dimes = (cents % 25) / 10; /*dimes are 10c */
+    int nickels = ((cents % 25) % 10) / 5; /*nickels are 5c */
+    int pennies = ((cents % 25) % 10) % 5; /*pennies are 1c */
 
     printf("%d\n", quarters + dimes + nickels + pennies);
+    return 0;
 }
