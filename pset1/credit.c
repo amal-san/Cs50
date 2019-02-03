4fa722eb3b59b996c28470f7fef6876afd12f733
Amalsanthoshp <amaldemaniax@gmail.com>
2019-02-03 14:41:37 +0530

    input

--- pset1/credit.c
+++ pset1/credit.c
@@ -1,1 +1,14 @@
-credit.cl
+#include <stdio.h>
+#include <string.h>
+ 
+int main(){
+
+	long long int a;
+	scanf("%lli",&a);
+	printf("%lli\n",a);
+	int num=0;
+	while(a>0) 
+	{
+	   num = a%10	
+    
+}
